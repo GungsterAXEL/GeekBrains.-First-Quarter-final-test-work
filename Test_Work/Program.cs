728e3c81efc15feeff571fcb4862f77f018a4160
Gun`gster <gungster.axel.ru@mail.ru>
2022-10-15 23:35:56 +0300

    Test work task and first version of program added

--- Test_Work/Program.cs
+++ Test_Work/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание: Написать программу, которая из имеющегося массива строк,
+// формирует массив из строк, длина которых меньше или равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2",":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+Console.Clear();
+
+string userText = Console.ReadLine();
+int size = 1, count = default;
+
+for (int i = 0; i < userText.Length; i++)
+{
+    if (userText[i] == ' ')
+    {
+        size++;
+    }
+}
+
+string[] textArray = new string[size];
+for (int i = 0; i < userText.Length; i++)
+{
+    if (userText[i] != ' ')
+    {
+        textArray[count] += userText[i];
+    }
+    else count++;
+}
+
+Console.Write("\n[");
+for (int i = 0; i < textArray.Length; i++)
+{
+    if (i < textArray.Length - 1) Console.Write($"\"{textArray[i]}\",");
+    else Console.Write($"\"{textArray[i]}\"");
+}
+Console.Write("]");
+
+count = default;
+Console.Write("[");
+for (int i = 0; i < textArray.Length; i++)
+{
+    if (textArray[i].Length < 4)
+    {
+        count++;
+        Console.Write($"\"{textArray[i]}\",");
+    }
+}
+Console.WriteLine("]");
