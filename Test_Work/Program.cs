9c602df59b3b71a22360c4ad9474a0c678a46f9f
Gun`gster <gungster.axel.ru@mail.ru>
2022-10-15 23:46:02 +0300

    Functions written

--- Test_Work/Program.cs
+++ Test_Work/Program.cs
@@ -8,6 +8,86 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
+string UserInput(string text, string message)
+{
+    Console.WriteLine(message);
+    while (string.IsNullOrWhiteSpace(text = Console.ReadLine()))
+    {
+        Console.WriteLine($"Строка не должна быть пустой!\n{message}");
+    }
+    return text;
+}
+
+string ExtraSpaceRemover(string text)
+{
+    while (text[0] == ' ') text = text.Remove(0, 1);
+    while (text[text.Length - 1] == ' ') text = text.Remove(text.Length - 1, 1);
+    for (int i = 1; i < text.Length - 1; i++)
+    {
+        while (text[i] == ' ' && text[i - 1] == ' ') text = text.Remove(i - 1, 1);
+    }
+    return text;
+}
+
+int StringArraySizer(string text)
+{
+    int size = 1;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == ' ')
+        {
+            size++;
+        }
+    }
+    return size;
+}
+
+string[] StringArrayMaker(string text, int size)
+{
+    int count = default;
+    string[] array = new string[size];
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] != ' ')
+        {
+            array[count] += text[i];
+        }
+        else count++;
+    }
+    return array;
+}
+
+void StringArrayPrinter(string[] text)
+{
+    Console.Write("\n[");
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (i < text.Length - 1) Console.Write($"\"{text[i]}\",");
+        else Console.Write($"\"{text[i]}\"");
+    }
+    Console.Write("]");
+}
+
+void CroppedStringArrayPrinter(string[] text)
+{
+    int count = default;
+    Console.Write("[");
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i].Length < 4)
+        {
+            count++;
+            Console.Write($"\"{text[i]}\",");
+        }
+    }
+    if (count > 0)
+    {
+        (int, int) CursorPosition = Console.GetCursorPosition();
+        Console.SetCursorPosition(CursorPosition.Item1 - 1, CursorPosition.Item2);
+    }
+    Console.WriteLine("]");
+}
+
 Console.Clear();
 
 string userText = Console.ReadLine();
